dc012a8d56d7339c46e7e88c6d2b22c41af4b843
Vlad <vladlapitsky37@gmail.com>
2023-02-25 17:30:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstNumber = +prompt('Enter first number');
+
+let secondNumber = +prompt('Enter second number');
+
+alert(`Sum equals to: ${firstNumber + secondNumber}. 
+Subtraction equals to: ${firstNumber - secondNumber}.
+Multiplication equals to: ${firstNumber * secondNumber}.
+Division equals to: ${firstNumber / secondNumber}`);  
