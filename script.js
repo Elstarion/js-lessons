ebc4d063ca0717e67dfe2707be20347569ccec08
Vlad <vladlapitsky37@gmail.com>
2023-03-02 07:08:43 +0200

    Conditional statements have been used

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let firstNumber = +prompt('Enter first number');
+let firstNumber = prompt('Enter first number');
 
-let secondNumber = +prompt('Enter second number');
+let secondNumber = prompt('Enter second number');
 
-alert(`Sum equals to: ${firstNumber + secondNumber}. 
-Subtraction equals to: ${firstNumber - secondNumber}.
-Multiplication equals to: ${firstNumber * secondNumber}.
-Division equals to: ${firstNumber / secondNumber}`);  
+if (firstNumber == null || secondNumber == null || firstNumber == '' || secondNumber == '') {
+    alert('One of the inputs is empty. Operation canceled.')
+} else {
+    alert(`Sum equals to: ${Number(firstNumber)  + Number(secondNumber)}`);
+    alert(`Multiplication equals to: ${firstNumber * secondNumber}`);
+
+    if (Number(firstNumber) < Number(secondNumber)) {
+        confirm('Are you sure you want to continue?') ? 
+        alert(`Subtraction equals to: ${firstNumber - secondNumber}`) : alert('Subtraction operation canceled');
+    } else alert(`Subtraction equals to: ${firstNumber - secondNumber}`)
+    
+    if (secondNumber == 0) {
+        alert(`You can't divide by 0`)
+    } else alert (`Division equals to: ${firstNumber / secondNumber}`);
+}
