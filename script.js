e6f2318eb1c6a14af79cdb784740a77f065a3052
Elstarion <vladlapitsky37@gmail.com>
2023-03-30 06:02:20 +0300

    lesson-27

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// let firstNumber = prompt('Enter first number');
-// let secondNumber = prompt('Enter second number');
+const firstNumber = prompt('Enter first number');
+const secondNumber = prompt('Enter second number');
 
 function isNumber(input) {
     if (isNaN(input) || !input) {
@@ -8,41 +8,146 @@ function isNumber(input) {
 }
 
 function calculate(firstNumber, secondNumber) {
-    const error = 'One of the inputs is not a number. Operation canceled.';
     
     if(isNumber(firstNumber) || isNumber(secondNumber)) {
-        return alert(error);
+        return getResult('One of the inputs is not a number. Operation canceled.', '');
     } else 
     sum(firstNumber, secondNumber),
     sub(firstNumber,secondNumber),
     mul(firstNumber,secondNumber),
-    div(firstNumber,secondNumber);   
+    div(firstNumber,secondNumber);
+}
+
+function getResult(string, result) {
+    const wrapper = document.querySelector('.wrapper');
+    const div = document.createElement('div');
+    div.classList.add('box');
+    wrapper.appendChild(div);
+
+    if (result > 0) {
+        div.style.backgroundColor = 'green';
+    } else if (result < 0 && result >= -50) {
+        div.style.backgroundColor = '#b20000';
+    } else if (result <= -50 && result >= -100) {
+        div.style.backgroundColor = '#990000';
+    } else if (result <= -100 && result >= -500) {
+        div.style.backgroundColor = '#7f0000';
+    } else if (result <= -500 && result > -Infinity) {
+        div.style.backgroundColor = '#4c0000';
+    }
+
+    return div.textContent = string + result;
 }
 
 function sum(firstNumber, secondNumber) {
-    alert(`Sum equals to: ${Number(firstNumber) + Number(secondNumber)}`);
+    const result = +firstNumber + +secondNumber;
+    return getResult('SUM = ', result);
 }
 
 function sub(firstNumber, secondNumber) {
-    if (firstNumber < secondNumber) {
+    const result = firstNumber - secondNumber;
+    if (+firstNumber < +secondNumber) {
         confirm('Are you sure you want to continue?') ?
-        alert(`Subtraction equals to: ${firstNumber - secondNumber}`) : alert('Subtraction operation canceled');
-    } else alert(`Subtraction equals to: ${firstNumber - secondNumber}`)
+        getResult('SUB = ', result) : getResult('Subtraction operation canceled.', '');
+    } else getResult('SUB = ', result);
 }
 
 function mul(firstNumber, secondNumber) {
-    alert(`Multiplication equals to: ${firstNumber * secondNumber}`); 
+    const result = firstNumber * secondNumber;
+    return getResult('MUL = ', result);
 }
 
 function div(firstNumber, secondNumber) {
+    const result = firstNumber / secondNumber;
     if (secondNumber == 0) {
-        alert(`You can't divide by 0`);
-    } else alert (`Division equals to: ${firstNumber / secondNumber}`);
+        getResult(`You can't divide by 0`, '');
+    } else getResult('DIV = ', result);
 }
 
-// calculate(firstNumber, secondNumber);
+calculate(firstNumber, secondNumber);
 
 // ---------------------------------------------------------------
+
+const numbers = ['js',5,46,32,true,7,83,[],16,75];
+
+function checkIndex(data) {
+    if (typeof data !== 'number') {
+        return true
+    } return false
+}
+
+function getMinNumber(arr) {
+    let min = Infinity;
+
+    for(let i = 0; i < arr.length; i++) {
+        if ( checkIndex(arr[i]) ) {
+            continue;
+        }
+
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return getResult('Minimum number is: ', min);
+}
+
+function getMaxNumber(arr) {
+    let max = -Infinity;
+
+    for (index of arr) {
+        if ( checkIndex(index) ) {
+            continue;
+        }
+
+        if (index > max) {
+            max = index;
+        }
+    }       
+
+    return getResult('Maximum number is: ', max);
+}
+
+function getSum(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if ( checkIndex(arr[i]) ) {
+            continue;
+        }
+        sum += arr[i];
+    }
+
+    return getResult('Sum is equal to: ', sum)
+}
+
+function tree() {
+    let hashtagTree = [];
+    let hash = '';
+
+    for (let i = 0; i < 5; i++) {
+        hashtagTree.push(hash += '#')
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+    const p = document.createElement('p');
+    p.classList.add('box');
+    p.classList.add('align');
+    wrapper.appendChild(p);
+
+    for (let i = 0; i < hashtagTree.length; i++) {
+        p.innerHTML += `${hashtagTree[i]}<br>`;
+    }
+        return p.innerHTML;
+}
+
+getMinNumber(numbers);
+getMaxNumber(numbers);
+getSum(numbers);
+tree();
+
+// ---------------------------------------------------------------
+
 
 function isObjectEmpty(object) {
     return Object.keys(object).length === 0
@@ -104,20 +209,12 @@ const a = new Accumulator(25);
 
 const b = new CancelableAccumulator(40);
 
-console.log(a.increment());
-console.log(a.increment());
-console.log(a.decrement());
-console.log('----------------');
-console.log(b.decrement());
-console.log(b.decrement());
-console.log(b.clear());
-console.log(b.increment());
-console.log(b.increment());
-
-
-
-
-
-
-
-
+// console.log(a.increment());
+// console.log(a.increment());
+// console.log(a.decrement());
+// console.log('----------------');
+// console.log(b.decrement());
+// console.log(b.decrement());
+// console.log(b.clear());
+// console.log(b.increment());
+// console.log(b.increment());
